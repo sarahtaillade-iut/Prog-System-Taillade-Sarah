4209712a8810c35e9205b94b7a62a89347a6dcfa
SARAH TAILLADE <sarah.taillade@iut.local>
2026-09-15 15:50:44 +0200

    Gradient et Image terminés.

---
+++ TP1_Lecture_Ecriture_Java/Gradient.java
@@ -0,0 +1,22 @@
+public class Gradient {
+    public static void main(String[] args) {
+        int largeur = 200;
+	int hauteur = 100;
+	Image img = new Image(largeur, hauteur);
+
+        // Génération du dégradé de bleu
+        for (int y = 0; y < hauteur; y++) {
+            for (int x = 0; x < largeur; x++) {
+                int bleu = (x * 255) / (largeur - 1);
+                img.setPixel(x, y, 0, 0, bleu);
+            }
+        }
+
+        try {
+            img.save_txt("gradient.ppm");
+            System.out.println("Dégradé créé avec succès !");
+        } catch (Exception e) {
+            System.err.println("Erreur lors de la création du dégradé : " + e.getMessage());
+        }
+    }
+}
